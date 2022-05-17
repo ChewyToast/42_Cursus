c35c0b59f16dd9b9b34cb1c15100c97d892feb6e
bruno <moll.bruno.99@gmail.com>
2022-05-17 02:48:09 +0200

    intentando las strchr

--- libft/ft_strchr.c
+++ libft/ft_strchr.c
@@ -10,16 +10,25 @@
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
+#include <stdio.h>
+#include <string.h>
 
 char	*ft_strchr(const char *s, int c)
 {
 	if (!s)
 		return (0);
-	while (*s != c)
+	while (*s != c && *s != '\0')
 		s++;
-	if (c == '\0')
+	if (*s == c || (*s == '\0' && c == '\0'))
 		return ((char *)s);
-	else if (*s == c)
-		return ((char *)s);
-	return (0);
+	else
+		return (0);
 }
+/*
+int	main(int av, char **argv)
+{
+	printf("EXPECTED: %s\n", strchr(argv[1], argv[2][0]));
+	printf("My: %s\n", ft_strchr(argv[1], argv[2][0]));
+	printf("EXPECTED: %s\n", strchr("0x55adad865020: \"teste\"", '\0'));
+        printf("My: %s\n", ft_strchr("0x55adad865020: \"teste\"", '\0'));
+}*/
