cca1383b9cd8c51405a52bf637e6af4f5f794530
ubuntu_MSI <bmoll-pe@student.42barcel>
2022-05-25 12:20:29 +0200

    TODO SUPER BIEN SIN TRAMPAS HASTA LSTLAST

--- libft/ft_strrspn.c
+++ libft/ft_strrspn.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_isalnum.c                                       :+:      :+:    :+:   */
+/*   ft_strrspn.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: marvin <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/11 11:59:35 by marvin            #+#    #+#             */
-/*   Updated: 2022/05/12 00:16:00 by marvin           ###   ########.fr       */
+/*   Updated: 2022/05/25 11:39:21 by bmoll-pe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
@@ -14,20 +14,9 @@
 size_t	ft_strrspn(const char *s, const char *accept)
 {
 	size_t	size;
-	size_t	tmp;
 
-	size = ft_strlen(s) - 1;
-	tmp = 0;
-	while (s[size] && size)
-	{
-		tmp = 0;
-		while (s[size] != accept[tmp] && accept[tmp])
-			tmp++;
-		if (s[size] == accept[tmp])
-			size--;
-		else
-			break ;
-	}
-	size++;
-	return (size);
+	size = ft_strlen((char *)s) - 1;
+	while (ft_strchr(accept, s[size]) && s[size])
+		size--;
+	return (ft_strlen(s) - size);
 }
