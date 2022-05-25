b12b084b2a014409883e09978708c1887a7e6335
ChewyToast <moll.bruno.99@gmail.com>
2022-05-25 12:20:29 +0200

    TODO SUPER BIEN SIN TRAMPAS HASTA LSTLAST

--- libft/ft_strspn.c
+++ libft/ft_strspn.c
@@ -6,7 +6,7 @@
 /*   By: marvin <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/11 11:59:35 by marvin            #+#    #+#             */
-/*   Updated: 2022/05/12 00:16:00 by marvin           ###   ########.fr       */
+/*   Updated: 2022/05/25 11:20:46 by bmoll-pe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
@@ -14,19 +14,9 @@
 size_t	ft_strspn(const char *s, const char *accept)
 {
 	size_t	size;
-	size_t	tmp;
 
 	size = 0;
-	tmp = 0;
-	while (s[size])
-	{
-		tmp = 0;
-		while (s[size] != accept[tmp] && accept[tmp])
-			tmp++;
-		if (s[size] == accept[tmp])
-			size++;
-		else
-			break ;
-	}
+	while (ft_strchr(accept, s[size]) && s[size])
+		size++;
 	return (size);
 }
--- libft/ft_strtrim.c
+++ libft/ft_strtrim.c
@@ -6,17 +6,13 @@
 /*   By: marvin <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/11 11:59:35 by marvin            #+#    #+#             */
-/*   Updated: 2022/05/25 02:23:38 by bmoll-pe         ###   ########.fr       */
+/*   Updated: 2022/05/25 12:15:19 by bmoll-pe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
 
 char	*ft_strtrim(char const *s1, char const *set)
 {
-	char	*str;
-
-	if (s1)
-		str = ft_substr(s1, ft_strspn(s1, set),
-				(ft_strrspn(s1, set) - ft_strspn(s1, set)));
-	return (str);
+	return (ft_substr(s1 + ft_strspn(s1, set), 0,
+			(ft_strlen(s1) - ft_strrspn(s1, set) - ft_strspn(s1, set) + 1)));
 }
