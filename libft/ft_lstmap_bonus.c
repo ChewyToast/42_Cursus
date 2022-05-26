a51cb63aafcd8592dd3130135f797c708b518cd8
ubuntu_MSI <bmoll-pe@student.42barcel>
2022-05-26 10:55:46 +0200

    todo libft con bonus menos paco -s lstmap

--- libft/ft_lstmap_bonus.c
+++ libft/ft_lstmap_bonus.c
@@ -6,7 +6,7 @@
 /*   By: bmoll-pe <bmoll-pe@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/26 02:42:01 by bmoll-pe          #+#    #+#             */
-/*   Updated: 2022/05/26 04:02:58 by bmoll-pe         ###   ########.fr       */
+/*   Updated: 2022/05/26 10:39:58 by bmoll-pe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
@@ -16,19 +16,23 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 	t_list	*flst;
 	t_list	*tmp;
 
+	if (!lst)
+		return (0);
 	flst = ft_lstnew(f(lst->content));
 	if (!flst)
 		return (0);
+	tmp = flst;
 	lst = lst->next;
 	while (lst)
 	{
-		flst->next = lst;
-		flst = ft_lstnew(f(lst->content));
-		if (!flst->content)
+		tmp->next = ft_lstnew(f(lst->content));
+		if (!(tmp->next))
 		{
-			del(dlst->content);
-			free(*);
+			ft_lstclear(&flst, del);
+			return (0);
 		}
+		tmp = tmp->next;
 		lst = lst->next;
 	}
+	return (flst);
 }
--- libft/ft_strtrim.c
+++ libft/ft_strtrim.c
@@ -6,13 +6,21 @@
 /*   By: marvin <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/11 11:59:35 by marvin            #+#    #+#             */
-/*   Updated: 2022/05/25 12:15:19 by bmoll-pe         ###   ########.fr       */
+/*   Updated: 2022/05/26 10:51:48 by bmoll-pe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
 
 char	*ft_strtrim(char const *s1, char const *set)
 {
-	return (ft_substr(s1 + ft_strspn(s1, set), 0,
-			(ft_strlen(s1) - ft_strrspn(s1, set) - ft_strspn(s1, set) + 1)));
+	size_t		size;
+
+	if (!s1 || !set)
+		return (NULL);
+	while (ft_strchr(set, *s1) && *s1)
+		s1++;
+	size = ft_strlen((char *)s1);
+	while (ft_strchr(set, s1[size]) && size)
+		size--;
+	return (ft_substr((char *)s1, 0, size + 1));
 }
