720f726a31a1cc5e08b8719d2ab17f081724b88b
ubuntu_MSI <bmoll-pe@student.42barcel>
2022-05-31 23:29:06 +0200

    pequeñas modificaciones en GNL

--- 01_get_next_line/get_next_line.h
+++ 01_get_next_line/get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: bmoll-pe <bmoll-pe@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/27 10:03:28 by bmoll-pe          #+#    #+#             */
-/*   Updated: 2022/05/31 00:19:37 by bmoll-pe         ###   ########.fr       */
+/*   Updated: 2022/05/31 23:25:55 by bmoll-pe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,12 +26,12 @@
 char	*get_next_line(const int fd);
 int		ft_clean(char *str);
 char	ft_check_str(char *str);
-int		trim_str(char *str);
+char	*trim_str(char *str);
 
 // --GNLU--
 
 char	*f_strjoin(char *s1, char *s2);
-int     get_line(char *line, char *str);
+char	*get_line(char *line, char *str);
 size_t	ft_strlen(const char *s);
 size_t	ft_strlcpy(char *dst, const char *src, size_t size);
 size_t	ft_strlcat(char *dst, const char *src, size_t size);
