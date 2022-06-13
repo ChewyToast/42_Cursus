151ff388aaabdc0a04d9d9ebf90449d042c036c0
ubuntu_MSI <bmoll-pe@student.42barcel>
2022-06-14 01:33:02 +0200

    imprime pero en segun que casos hay leaks de memoria y si el archivo esta vacio no funciona correctamente

--- 01_ft_get_next_line/get_next_line.h
+++ 01_ft_get_next_line/get_next_line.h
@@ -1,6 +1,12 @@
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
+# include <stdarg.h>
+# include <strings.h>
+# include <stddef.h>
+# include <unistd.h>
+# include <stdio.h>
+# include <string.h>
 # include <stdlib.h>
 # include <fcntl.h>
 
@@ -10,11 +16,13 @@
 
 char	*get_next_line(const int fd);
 
-ssize_t check_nl(const char *str);
-char	*ft_read(const int fd);
+char	*copy_line(char *buff);
+size_t check_nl(const char *str);
+char	*ft_read(const int fd, char *saved_buff);
 
 char	*ft_strjoin(char *read_buff, char *tmp);
 size_t	ft_strlcat(char *dst, const char *src, size_t size);
 size_t	ft_strlen(const char *s);
+char	*ft_substr(char *read_buff, size_t start, size_t len);
 
 #endif
--- 01_ft_get_next_line/get_next_line_utils.c
+++ 01_ft_get_next_line/get_next_line_utils.c
@@ -4,12 +4,13 @@ char	*ft_strjoin(char *read_buff, char *tmp)
 {
 	char	*str;
 	size_t	size;
+	size_t	i;
 
-	size = ft_strlen(s1) + ft_strlen(s2);
+	i = 0;
+	size = ft_strlen(read_buff) + ft_strlen(tmp);
 	str = malloc(sizeof(char) * (size + 1));
 	if (str == NULL)
 		return (0);
-	size = ft_strlen(s1);
 	while (size && read_buff[i])
 	{
 		str[i] = read_buff[i];
@@ -18,8 +19,7 @@ char	*ft_strjoin(char *read_buff, char *tmp)
 	}
 	str[i] = '\0';
 	free(read_buff);
-	ft_strlcat(str, tmp, size + 1);
-	free(tmp);
+	ft_strlcat(str, tmp, 0xffffffff);
 	return (str);
 }
 
@@ -52,11 +52,49 @@ size_t	ft_strlcat(char *dst, const char *src, size_t size)
 	return (return_value);
 }
 
-size_t	ft_strlen(const char *s)
+char	*ft_substr(char *read_buff, size_t start, size_t len)
 {
-	int	i;
+	char	*str;
+	size_t	i;
 
 	i = 0;
+	len = ft_strlen(read_buff) - check_nl(read_buff);
+	str = malloc(sizeof(char) * (len + 1));
+	if (!str)
+		return (0);
+	str[len] = '\0';
+	len++;
+	while (len && read_buff[i + start])
+	{
+		str[i] = read_buff[i + start];
+		i++;
+		len--;
+	}
+	free(read_buff);
+	return (str);
+}
+
+size_t	check_nl(const char *str)
+{
+	size_t	size;
+
+	size = 0;
+	if (!str)
+		return (0);
+	while (str[size] && str[size] != '\n')
+		size++;
+	if (!str[size])
+		return (0);
+	return (size);
+}
+
+size_t	ft_strlen(const char *s)
+{
+	size_t	i;
+
+	i = 0;
+	if (!s)
+		return (0);
 	while (s[i] != '\0')
 		i++;
 	return (i);
