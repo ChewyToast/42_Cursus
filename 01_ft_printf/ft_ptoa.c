38dcd405bb3471ab77c3378c6fc44b793128de97
ubuntu_MSI <bmoll-pe@student.42barcel>
2022-06-15 00:07:48 +0200

    paco -s?

--- 01_ft_printf/ft_ptoa.c
+++ 01_ft_printf/ft_ptoa.c
@@ -26,8 +26,6 @@ static ssize_t	sizeofint(unsigned long long int n)
 
 static char	*ptoa_extract(unsigned long long int n, char *str, ssize_t size)
 {
-	ft_putchar('0');
-	ft_putchar('x');
 	while (--size >= 0)
 	{
 		str[size] = ("0123456789abcdef"[n % 16]);
@@ -43,12 +41,18 @@ ssize_t	ft_ptoa(unsigned long long int n)
 
 	if (n == 0)
 		return (ft_putstr("0x0"));
+	if (ft_putchar('0') < 0)
+		return (0);
+	if (ft_putchar('x') < 0)
+		return (0);
 	size = sizeofint(n);
 	str = malloc(sizeof(char) * (size + 1));
 	if (!str)
 		return (-1);
 	str[size] = '\0';
 	str = ptoa_extract(n, str, size);
+	if (!str)
+		return (-1);
 	size = ft_putstr(str) + 2;
 	free(str);
 	return (size);
