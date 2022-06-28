ed20c6496919d64fce26d17b32279c06853b65c1
ubuntu_MSI <bmoll-pe@student.42barcel>
2022-06-28 02:14:24 +0200

    fill stack a completed :)

--- 02_ft_push_swap/source/push_swap.h
+++ 02_ft_push_swap/source/push_swap.h
@@ -16,18 +16,30 @@
 
 typedef struct s_stack
 {
-	int				content;
+	int				num;
 	struct s_stack	*next;
 }	t_stack;
 
 /*FT_CHECK_INPUT*/
-// FIRST STEP, SEE THE CHARS OF THE INPUT
+// function chain to check the input
 size_t	check_input(char **input);
+// function to show the error msg and return 0
+size_t	ft_error(void);
 
 /*STACK_FUNCTIONS*/
-//FUNCTION CREATES A T_STACK WITH THE VALUE OF THE PARAMETER
+// function fills the stack_a with the input
+size_t	fill_stack(char **input, t_stack **stack_a);
+// function creates a new stack item with the num of the parametrer
 t_stack	*create_cont(int num);
-//FUNCTION ADDS AT THE END OF THE STACK, A NEW NUMBER
-void	add_num(t_stack **lst, t_stack *nlst);
+// function adds at the end of the stack a new item
+size_t	add_at_end(t_stack **stack, t_stack *new_item);
+// function to free all the stack
+void	ft_free(t_stack **stack);
+// function to see the stack, just a checker for the programer
+void	show_stack(t_stack **stack);
+
+/*STACK_OPERATIONS*/
+// function adds at the top of the stack the first item of other stack
+void	ft_pa(t_stack **stack_a, t_stack **stack_b);
 
 #endif
