4f1caccb5621184a1c344199881b7aafd2ff9ab5
ChewyToast <moll.bruno.99@gmail.com>
2022-07-07 12:20:52 +0200

    we have the # bonus maded

--- 01_ft_printf/ft_printf_bonus/ft_10to16_bonus.c
+++ 01_ft_printf/ft_printf_bonus/ft_10to16_bonus.c
@@ -10,7 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "ft_printf.h"
+#include "ft_printf_bonus.h"
 
 static ssize_t	sizeofint(unsigned long long int n)
 {
